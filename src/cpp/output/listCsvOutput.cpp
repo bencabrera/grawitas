53a9ee43960f61d13554e0acb3f89a8e41437e08
Benjamin Cabrera <benny@bcabrera.de>
2017-03-26 16:13:14 +0200

    Improved csv format output

--- src/cpp/output/listCsvOutput.cpp
+++ src/cpp/output/listCsvOutput.cpp
@@ -1,24 +1,38 @@
 #include "listCsvOutput.h"
 
 #include <algorithm>
+#include <boost/algorithm/string/replace.hpp>
 #include "dateOutput.h"
 
 namespace Grawitas {
 
-	void listToCsv(std::ostream& ostr, const ParsedTalkPage& parsedTalkPage)
+	std::string csv_encode(const std::string& str)
 	{
-		for (auto paragraph : parsedTalkPage) {
-			for(auto it = paragraph.second.begin(); it != paragraph.second.end(); ++it)
+		auto rtn = boost::replace_all_copy(str, "\"", "\"\"");
+		boost::replace_all(rtn, "\n", "\\n");
+		return rtn;
+	}
+
+	void listToCsv(std::ostream& ostr, const ParsedTalkPage& parsed_talk_page)
+	{
+		constexpr char CSV_DELIMITER = ',';
+
+		ostr
+			<< "Comment Id" << CSV_DELIMITER
+			<< "Parent Id" << CSV_DELIMITER
+			<< "User" << CSV_DELIMITER
+			<< "Date" << CSV_DELIMITER
+			<< "Text" << std::endl;
+
+		for (auto section : parsed_talk_page) {
+			for(const auto& comment : section.second)
 			{
-				auto comment = *it;
-				auto text = comment.Text;
-				std::replace( text.begin(), text.end(), ';', '.');
 				ostr
-					<< comment.Id << ";"
-					<< comment.ParentId << ";"
-					<< comment.User << ";"
-					<< to_iso_8601(comment.Date) << ";"
-					<< text << std::endl;
+					<< comment.Id << CSV_DELIMITER
+					<< comment.ParentId << CSV_DELIMITER
+					<< "\"" << csv_encode(comment.User) << "\"" << CSV_DELIMITER
+					<< "\"" << to_iso_8601(comment.Date) << "\"" << CSV_DELIMITER
+					<< "\"" << csv_encode(comment.Text) << "\"" << std::endl;
 			}
 		}
 	}
